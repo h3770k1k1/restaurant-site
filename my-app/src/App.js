02d7306448efe687cbeb4add566fbddfaf1e8d0f
h3770k1k1 <amagdarass@gmail.com>
2023-01-22 19:11:17 +0100

    add few main page elements

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import { React } from "react";
-import "./styles.css";
+import "./App.css";
+import mainLogo from "./logo.png";
+import mainPhoto from "./main-photo.png";
 
 function App() {
 	return (
 		<div className="MainContainer">
 			<div className="Navbar">
 				<div className="logo-container">
-					<img className="logo-img" src={"logo.png"} />
+					<img className="logo-img" src={mainLogo} />
 				</div>
-				<div>
+				<div className="nav-buttons-container">
 					<div className="nav-button">MENU</div>
 					<div className="nav-button">GALLERY</div>
 					<div className="nav-button">CONTACT</div>
@@ -16,7 +18,17 @@ function App() {
 				<div className="nav-button login-button">SIGN IN</div>
 			</div>
 			<div className="main-image-container">
-				<img src={"main-photo.png"} />
+				<div className="interactions-container">
+					<div className="text-container">
+						<div className="title">KEBUG KEBAB</div>
+						<div className="greeting">Welcome to our restaurant!</div>
+					</div>
+					<div className="order-buttons-container">
+						<div className="order-button">ORDER ON-LINE</div>
+						<div className="order-button">CALL AND ORDER</div>
+					</div>
+				</div>
+				<img className="main-photo-img" src={mainPhoto} />
 			</div>
 		</div>
 	);
