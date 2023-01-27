4dc4dd9b495eecd1c0f3ba72c7cab567335406cc
h3770k1k1 <amagdarass@gmail.com>
2023-01-27 16:51:38 +0100

    fixed buttons bug move navbar  to different file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,12 @@
 import { React } from "react";
-import "./App.css";
-import mainLogo from "./logo.png";
+import "./App.scss";
+import { Navbar } from "./navbar";
 import mainPhoto from "./main-photo.png";
 
 function App() {
 	return (
 		<div className="MainContainer">
-			<div className="Navbar">
-				<div className="logo-container">
-					<img className="logo-img" src={mainLogo} />
-				</div>
-				<div className="nav-buttons-container">
-					<div className="nav-button">MENU</div>
-					<div className="nav-button">GALLERY</div>
-					<div className="nav-button">CONTACT</div>
-				</div>
-				<div className="nav-button login-button">SIGN IN</div>
-			</div>
+			<Navbar />
 			<div className="main-image-container">
 				<div className="interactions-container">
 					<div className="text-container">
