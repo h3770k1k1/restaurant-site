ebd59ec50d39f7b8a3e4f6adb719684456d1fd49
h3770k1k1 <amagdarass@gmail.com>
2023-01-27 21:00:48 +0100

    add footer file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
 import { React } from "react";
 import "./App.scss";
+import "./footer.css";
 import { Navbar } from "./navbar";
+import { Footer } from "./footer";
 import mainPhoto from "./main-photo.png";
 
 function App() {
 	return (
 		<div className="MainContainer">
 			<Navbar />
-			<div className="main-image-container">
-				<div className="interactions-container">
-					<div className="text-container">
-						<div className="title">KEBUG KEBAB</div>
-						<div className="greeting">Welcome to our restaurant!</div>
-					</div>
-					<div className="order-buttons-container">
-						<div className="order-button">ORDER ON-LINE</div>
-						<div className="order-button">CALL AND ORDER</div>
-					</div>
+			<div className="interactions-container">
+				<div className="text-container">
+					<div className="title">KEBUG KEBAB</div>
+					<div className="greeting">Welcome to our restaurant!</div>
 				</div>
-				<img className="main-photo-img" src={mainPhoto} />
+				<div className="order-buttons-container">
+					<div className="order-button">ORDER ON-LINE</div>
+					<div className="order-button">CALL AND ORDER</div>
+				</div>
 			</div>
+			<img className="main-photo-img" src={mainPhoto} />
+			<Footer />
 		</div>
 	);
 }
