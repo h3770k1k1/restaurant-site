9156937a60205390e1fa9ac09f395360ed78cd91
h3770k1k1 <amagdarass@gmail.com>
2023-01-27 16:51:38 +0100

    fixed buttons bug move navbar  to different file

---
+++ my-app/src/navbar.js
@@ -0,0 +1,18 @@
+import { React } from "react";
+import mainLogo from "./logo.png";
+
+export const Navbar = () => {
+	return (
+		<div className="Navbar">
+			<div className="logo-container">
+				<img className="logo-img" src={mainLogo} />
+			</div>
+			<div className="nav-buttons-container">
+				<div className="nav-button">MENU</div>
+				<div className="nav-button">GALLERY</div>
+				<div className="nav-button">CONTACT</div>
+			</div>
+			<div className="nav-button login-button">SIGN IN</div>
+		</div>
+	);
+};
