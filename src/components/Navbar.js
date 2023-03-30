e59acbcb66d8d109ba8dbc3de09f6a9a8f28fe62
h3770k1k1 <amagdarass@gmail.com>
2023-03-30 20:36:22 +0200

    contact subpage is working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import { React } from "react";
 import mainLogo from "../resources/logo.png";
 import dropDownArrow from "../resources/drop-down-arrow.svg";
-import { Link } from "react-router-dom";
 import { Suspense } from "react";
 import { useState } from "react";
 import "../App.scss";
 import { useTranslation } from "react-i18next";
+import { Link, Route, NavLink } from "react-router-dom";
+import Contact from "./Contact";
 
 export const Navbar = () => {
 	const { t, i18n } = useTranslation();
@@ -21,7 +22,9 @@ export const Navbar = () => {
 			<div className="nav-buttons-container">
 				<div className="nav-button">MENU</div>
 				<div className="nav-button">GALLERY</div>
-				<div className="nav-button">CONTACT</div>
+				<NavLink to="/contact" className="nav-button">
+					CONTACT
+				</NavLink>
 			</div>
 			<div className="nav-button login-button">SIGN IN</div>
 			<div className="language-change-button">
