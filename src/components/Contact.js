38536ff43559b5bbbdec3cc4e43732945b21c423
h3770k1k1 <amagdarass@gmail.com>
2023-04-03 20:38:38 +0200

    wireframe main elements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import "./Contact.css";
 
 const Contact = () => {
-	return <div>hello</div>;
+	return (
+		<div className="MainContainer">
+			<Navbar />
+			<div className="contact-data-container">
+				<div className="contact-info"></div>
+				<div className="map-block">
+					<div className="e-mail-container">
+						<div className="contact-form">
+							<div className="text-block"></div>
+							<div className="message-box"></div>
+							<div className="signature"></div>
+						</div>
+					</div>
+					<div className="map-container"></div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Contact;
