7e9d6036527d53da3a21cf54ee53a97a43623eee
h3770k1k1 <amagdarass@gmail.com>
2023-05-10 20:09:20 +0200

    distance from the user alert done

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
-import "./map.css";
-function Map() {
+import "./Contact.css";
+
+function Contact() {
   const mapRef = useRef(null);
 
   const initMap = () => {
@@ -15,13 +16,66 @@ function Map() {
     })
   };
 
+
+  const checkDistance = (userLocation) => {
+    const location = { lat: 52.2298, lng: 21.0118 };
+    const distance = calculateDistance(userLocation, location);
+
+    if (distance > 10) {
+      alert("Jesteś oddalony o ponad 10 km od naszej lokalizacji! Dowóz niemożliwy!");
+    }if (distance > 10) {
+      alert("Jesteś oddalony o mniej niż 10 km od naszej lokalizacji! Możemy dostarczyć Twoje zamówienie!");
+    }
+    
+  };
+
+  const calculateDistance = (point1, point2) => {
+    const earthRadius = 6371; // Promień ziemi
+    const lat1 = toRadians(point1.lat);
+    const lat2 = toRadians(point2.lat);
+    const lon1 = toRadians(point1.lng);
+    const lon2 = toRadians(point2.lng);
+
+    const dlat = lat2 - lat1;
+    const dlon = lon2 - lon1;
+
+    const a =
+      Math.sin(dlat / 2) ** 2 +
+      Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadius * c;
+
+    return distance;
+  };
+
+  const toRadians = (angle) => {
+    return (angle * Math.PI) / 180;
+  };
+
   useEffect(() => {
     const script = document.createElement("script");
-    const API_KEY = 'config.js';
+    const API_KEY = 'key.js';
     script.src = `https://maps.googleapis.com/maps/api/js?key=&{API_KEY}&callback=initMap`;
     script.async = true;
     window.initMap = initMap;
     document.body.appendChild(script);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const userLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          checkDistance(userLocation);
+        },
+        (error) => {
+          console.error("Błąd podczas pobierania lokalizacji użytkownika:", error);
+        }
+      );
+    } else {
+      console.error("Geolokalizacja nie jest obsługiwana przez przeglądarkę.");
+    }
   }, []);
 
   return (
@@ -31,4 +85,4 @@ function Map() {
   );
 }
 
-export default Map;
+export default Contact;
